35c0b2841416d27dafda0c5056657660e83e46a9
22050908-Meng Pei <2398696309@qq.com>
2023-07-04 21:19:15 +0800

    移植nemu的macro和common

--- verilator/include/common.h
+++ verilator/include/common.h
@@ -5,14 +5,18 @@
 #include <iostream>
 #include <cstdio>
 #include <cassert>
+#include <inttypes.h>
 
 #include "debug.h"
 #include "utils/log.h"
 
 typedef uint64_t word_t;
 typedef int64_t sword_t;
+#define FMT_WORD "0x%016" PRIx64
 
 typedef word_t vaddr_t;
 typedef uint64_t paddr_t;
+#define FMT_PADDR "0x%016" PRIx64
+typedef uint16_t ioaddr_t;
 
 #endif
