9edacb1f001b3103cb3ec38fbac11a7c32ec2319
22050908-Meng Pei <2398696309@qq.com>
2023-07-04 14:19:26 +0800

    selectreg

--- playground/src/register/SelectorPC.scala
+++ playground/src/register/SelectorPC.scala
@@ -7,7 +7,7 @@ class SelectorPC extends Module {
   val io = IO(new Bundle {
     val opcode = Input(UInt(7.W))
     val branch_out = Input(Bool())
-    val pc = Input(UInt(64.W))
+    val snpc = Input(UInt(64.W))
     val alu_out = Input(UInt(64.W))
 
     val next_pc = Output(UInt(64.W))
@@ -15,8 +15,7 @@ class SelectorPC extends Module {
 
   val op_j = (io.opcode === "b1101111".U) || (io.opcode === "b1100111".U)
 
-  val snpc = Mux(io.opcode(1, 0) === "b11".U, io.pc + 4.U(64.W), io.pc)
   val dnpc = Cat(io.alu_out(63, 1), 0.U(1.W))
 
-  io.next_pc := Mux(io.branch_out || op_j, dnpc, snpc)
+  io.next_pc := Mux(io.branch_out || op_j, dnpc, io.snpc)
 }
--- playground/test/src/register/SelectorPCTest.scala
+++ playground/test/src/register/SelectorPCTest.scala
@@ -11,7 +11,7 @@ object SelectorPCTest extends ChiselUtestTester {
         dut.io.opcode.poke("b1101111".U)
         dut.io.branch_out.poke(false.B)
         dut.io.alu_out.poke("h02".U)
-        dut.io.pc.poke("h8".U)
+        dut.io.snpc.poke("h8".U)
         dut.clock.step()
         dut.io.next_pc.expect("h02".U)
       }
@@ -21,7 +21,7 @@ object SelectorPCTest extends ChiselUtestTester {
         dut.io.opcode.poke("b1100111".U)
         dut.io.branch_out.poke(false.B)
         dut.io.alu_out.poke("h3".U)
-        dut.io.pc.poke("hc".U)
+        dut.io.snpc.poke("hc".U)
         dut.clock.step()
         dut.io.next_pc.expect("h2".U)
       }
