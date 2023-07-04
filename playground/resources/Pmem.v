79c470c1654cdfdcc0c17952a3e1496184daebc7
22050908-Meng Pei <2398696309@qq.com>
2023-07-04 17:19:12 +0800

    fix mem dpi-c

--- playground/resources/Pmem.v
+++ playground/resources/Pmem.v
@@ -9,8 +9,8 @@ module Pmem(
 );
 
   import "DPI-C" function void mem(input mem_en, input w_en, input signed_en,
-                                    input logic [63:0] addr, input logic [63:0] w_data,
-                                  input logic [63:0] r_mask, output logic [63:0] r_data);
+                                    input longint addr, input longint w_data,
+                                  input longint r_mask, output longint r_data);
 
   always @(*) begin
     mem(mem_en, w_en, signed_en, addr, w_data, r_mask, r_data);
--- verilator/src/memory/memory.cpp
+++ verilator/src/memory/memory.cpp
@@ -15,8 +15,8 @@ extern VTop *top;
 static bool pre_clock;
 
 void mem(const svLogic mem_en, const svLogic w_en, const svLogic signed_en,
-         const svLogicVecVal *addr, const svLogicVecVal *w_data,
-         const svLogicVecVal *r_mask, svLogicVecVal *r_data) {
+         const long long addr, const long long w_data,
+         const long long r_mask, long long *r_data) {
   if (top->clock == 1) {
     if (top->clock == pre_clock) {
       return;
@@ -28,29 +28,29 @@ void mem(const svLogic mem_en, const svLogic w_en, const svLogic signed_en,
   }
 
   if (mem_en == 0) {
-    *((uint64_t *)r_data) = *((uint64_t *)addr);
+    *((uint64_t *)r_data) = addr;
     return;
   }
 
   Log(ANSI_FMT("MEM", ANSI_FG_RED));
 
-  uint64_t mask = *((uint64_t *)r_mask);
+  uint64_t mask = r_mask;
 
-  if (w_en) {
-    uint64_t data = *((uint64_t *)w_data);
+  if (w_en == 1) {
+    uint64_t data = w_data;
 
     switch (mask) {
       case 0x0ffl:
-        vaddr_write(*((uint64_t *)addr), 1, data);
+        vaddr_write(addr, 1, data);
         break;
       case 0x0ffffl:
-        vaddr_write(*((uint64_t *)addr), 2, data);
+        vaddr_write(addr, 2, data);
         break;
       case 0x0ffffffffl:
-        vaddr_write(*((uint64_t *)addr), 4, data);
+        vaddr_write(addr, 4, data);
         break;
       case 0xffffffffffffffffl:
-        vaddr_write(*((uint64_t *)addr), 8, data);
+        vaddr_write(addr, 8, data);
         break;
       default:
         break;
@@ -60,19 +60,19 @@ void mem(const svLogic mem_en, const svLogic w_en, const svLogic signed_en,
 
     switch (mask) {
       case 0x0ffl:
-        data = vaddr_read(*((uint64_t *)addr), 1);
+        data = vaddr_read(addr, 1);
         if (signed_en == 1) data = (int8_t)data;
         break;
       case 0x0ffffl:
-        data = vaddr_read(*((uint64_t *)addr), 2);
+        data = vaddr_read(addr, 2);
         if (signed_en == 1) data = (int16_t)data;
         break;
       case 0x0ffffffffl:
-        data = vaddr_read(*((uint64_t *)addr), 4);
+        data = vaddr_read(addr, 4);
         if (signed_en == 1) data = (int32_t)data;
         break;
       case 0xffffffffffffffffl:
-        data = vaddr_read(*((uint64_t *)addr), 8);
+        data = vaddr_read(addr, 8);
         break;
       default:
         data = 0;
