97934f7e6074d4e76476afeb2ef3da3ed5b1be74
22050908-Meng Pei <2398696309@qq.com>
2023-07-10 15:20:58 +0800

    重写difftest的寄存器对比

--- verilator/src/simulate/difftest.cpp
+++ verilator/src/simulate/difftest.cpp
@@ -22,36 +22,25 @@ void dl_init(const char *dl_file) {
 }
 
 static bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  bool flag = true;
+  extern const char *regs[];
 
   for (int i = 0; i < 32; ++i) {
-    if (cpu.gpr[i] != ref_r->gpr[i]) {
-      flag = false;
-      break;
+    if(!difftest_check_reg(regs[i], pc, ref_r->gpr[i], cpu.gpr[i])) {
+      return false;
     }
   }
 
-  if (cpu.pc != ref_r->pc) {
-    flag = false;
+  if(!difftest_check_reg("pc", pc, ref_r->pc, cpu.pc)) {
+    return false;
   }
 
-  if (!flag) {
-    printf(ANSI_FMT("Something wrong at pc " FMT_WORD, ANSI_FG_YELLOW) "\n", pc);
-  }
-
-  return flag;
+  return true;
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
-    for (int i = 0; i < 32; ++i) {
-      printf("ref %d\t0x%016lx\n", i, ref->gpr[i]);
-    }
-    printf("ref pc \t0x%016lx\n", ref->pc);
-
     npc_state.state = NPC_ABORT;
     npc_state.halt_pc = pc;
-    dump_isa();
   }
 }
 
