de9c8afd7d2f69f50d249b5b4c54e70dc7ffe33d
22050908-Meng Pei <2398696309@qq.com>
2023-07-26 10:59:04 +0800

    寄存器堆

--- playground/src/utils/Register.scala
+++ playground/src/utils/Register.scala
@@ -2,8 +2,25 @@ package utils
 
 import chisel3._
 
-object RegisterBundle extends Bundle {
+class RegisterIO extends Bundle {
+  val rs1 = Input(UInt(5.W))
+  val rs2 = Input(UInt(5.W))
+  val rd = Input(UInt(5.W))
+
+  val w_en = Input(Bool())
+  val w_data = Input(UInt(64.W))
+
+  val r_data1 = Output(UInt(64.W))
+  val r_data2 = Output(UInt(64.W))
 }
 
 class Register extends Module {
+  val io = IO(new RegisterIO())
+
+  val reg_files = RegInit(VecInit(Seq.fill(32)(0.U(64.W))))
+
+  reg_files(io.rd) := Mux(io.rd.orR && io.w_en, io.w_data, reg_files(io.rd))
+
+  io.r_data1 := reg_files(io.rs1)
+  io.r_data2 := reg_files(io.rs2)
 }
---
+++ playground/test/src/utils/RegisterTest.scala
@@ -0,0 +1,87 @@
+package utils
+
+import chisel3._
+import chiseltest._
+import utest._
+
+object RegisterTest extends ChiselUtestTester {
+  val tests = Tests {
+    test("Register") {
+      test("readRS1") {
+        testCircuit(new Register()) { dut =>
+          // init, write 0x1234 to x1, read x1 from rs1
+          dut.io.rd.poke("h1".U)
+          dut.io.rs1.poke("h1".U)
+          dut.io.rs2.poke("h0".U)
+          dut.io.w_en.poke(true.B)
+          dut.io.w_data.poke("h1234".U)
+          dut.clock.step()
+          dut.io.r_data1.expect("h1234".U)
+          dut.io.r_data2.expect("h0".U)
+        }
+      }
+      test("readRS2") {
+        testCircuit(new Register()) { dut =>
+          // init, write 0x13456 to x2, read x2 from rs2
+          dut.io.rd.poke("h2".U)
+          dut.io.rs1.poke("h0".U)
+          dut.io.rs2.poke("h2".U)
+          dut.io.w_en.poke(true.B)
+          dut.io.w_data.poke("h13456".U)
+          dut.clock.step()
+          dut.io.r_data1.expect("h0".U)
+          dut.io.r_data2.expect("h13456".U)
+        }
+      }
+      test("writeRD") {
+        testCircuit(new Register()) { dut =>
+          // init, write 0x11 to x17, and try to read x17 at this time
+          dut.io.rd.poke("h11".U)
+          dut.io.rs1.poke("h11".U)
+          dut.io.rs2.poke("h0".U)
+          dut.io.w_en.poke(true.B)
+          dut.io.w_data.poke("h11".U)
+          dut.clock.step()
+          dut.io.r_data1.expect("h11".U)
+          dut.io.r_data2.expect("h0".U)
+        }
+      }
+      test("writeRD2times") {
+        testCircuit(new Register()) { dut =>
+          // init, write 0xffff to x10, read x10 from both rs1 and rs2
+          dut.io.rd.poke("ha".U)
+          dut.io.rs1.poke("ha".U)
+          dut.io.rs2.poke("ha".U)
+          dut.io.w_en.poke(true.B)
+          dut.io.w_data.poke("hffff".U)
+          dut.clock.step()
+          dut.io.r_data1.expect("hffff".U)
+          dut.io.r_data2.expect("hffff".U)
+
+          // disable write 0x7890 to x10 at this time, read x10 from both rs1 and rs2
+          dut.io.rd.poke("ha".U)
+          dut.io.rs1.poke("ha".U)
+          dut.io.rs2.poke("ha".U)
+          dut.io.w_en.poke(false.B)
+          dut.io.w_data.poke("h7890".U)
+          dut.clock.step()
+          dut.io.r_data1.expect("hffff".U)
+          dut.io.r_data2.expect("hffff".U)
+        }
+      }
+      test("writeZero") {
+        testCircuit(new Register()) { dut =>
+          // init, write 0xcdcd to x0
+          dut.io.rd.poke(0.U)
+          dut.io.rs1.poke(0.U)
+          dut.io.rs2.poke(0.U)
+          dut.io.w_en.poke(true.B)
+          dut.io.w_data.poke("hcdcd".U)
+          dut.clock.step()
+          dut.io.r_data1.expect(0.U)
+          dut.io.r_data2.expect(0.U)
+        }
+      }
+    }
+  }
+}
