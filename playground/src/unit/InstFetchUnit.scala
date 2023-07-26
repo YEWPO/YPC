1a22e0bd48fea477b6b8dfe53b1cfd5730594513
22050908-Meng Pei <2398696309@qq.com>
2023-07-26 16:29:15 +0800

    取指单元

--- playground/src/unit/InstFetchUnit.scala
+++ playground/src/unit/InstFetchUnit.scala
@@ -1,9 +1,33 @@
 package unit
 
 import chisel3._
+import utils._
 
 class InstFetchUnitIO extends Bundle {
+  val npc = Input(UInt(64.W))
+
+  val pc_f = Output(UInt(64.W))
+  val inst = Output(UInt(32.W))
+  val snpc = Output(UInt(64.W))
 }
 
 class InstFetchUnit extends Module {
+  val io = IO(new InstFetchUnitIO())
+
+  val program_counter = RegInit("h8000_0000".U)
+  val inst_mem = Module(new InstMem())
+
+  /**
+    * pc = npc
+    */
+  program_counter := io.npc
+
+  /**
+    * inst = mem[pc]
+    * snpc = pc + 4
+    */
+  inst_mem.io.addr := program_counter
+  io.inst := inst_mem.io.inst
+  io.pc_f := program_counter
+  io.snpc := program_counter + 4.U
 }
