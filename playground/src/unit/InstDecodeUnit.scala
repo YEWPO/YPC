4fe4e645d1e29d63a4eb862f0cc76152277e85e0
22050908-Meng Pei <2398696309@qq.com>
2023-07-27 14:51:15 +0800

    指令初始化为NOP

--- playground/src/unit/InstDecodeUnit.scala
+++ playground/src/unit/InstDecodeUnit.scala
@@ -53,7 +53,7 @@ class InstDecodeUnit extends Module {
   val invalid_part = Module(new Invalid)
 
   withReset(io.reset) {
-    val inst_f = RegEnable(io.inst_f, 0.U(32.W), io.enable)
+    val inst_f = RegEnable(io.inst_f, "h13".U(32.W), io.enable)
     val pc_f   = RegEnable(io.pc_f, "h8000_0000".U(64.W), io.enable)
     val snpc_f = RegEnable(io.snpc_f, "h8000_0000".U(64.W), io.enable)
 
