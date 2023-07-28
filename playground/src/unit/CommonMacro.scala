c4862568ff2fa17179b8a0fb316e71652e2e6e0c
22050908-Meng Pei <2398696309@qq.com>
2023-07-28 17:32:11 +0800

    添加了一个扩展通用方法

--- playground/src/unit/CommonMacro.scala
+++ playground/src/unit/CommonMacro.scala
@@ -1,8 +1,15 @@
 package unit
 
 import chisel3._
+import chisel3.util._
 
 object CommonMacro {
-  val PC_RESET_VAL = "h8000_0000".U(64.W)
+  val PC_RESET_VAL   = "h8000_0000".U(64.W)
   val INST_RESET_VAL = "h13".U(32.W) // NOP instruction
+
+  def signExtend(src: UInt): UInt = {
+    val src_len    = src.getWidth
+    val extend_len = 64 - src_len
+    Cat(Fill(extend_len, src(src_len - 1)), src)
+  }
 }
--- playground/src/utils/ImmGen.scala
+++ playground/src/utils/ImmGen.scala
@@ -3,6 +3,7 @@ package utils
 import chisel3._
 import chisel3.util._
 import control._
+import unit._
 
 class ImmGenIO extends Bundle {
   val in       = Input(UInt(25.W))
@@ -20,28 +21,27 @@ class ImmGen extends Module {
     * I,S,B,U,J type immediate needs signed extend
     *
     * +-------------+
-    * | inst[31:20] | I-immediate 12-bits
+    * | inst[31:20] | I-immediate
     * +-------------+
     * +-------------+------------+
-    * | inst[31:25] | inst[11:7] | S-immediate 12-bits
+    * | inst[31:25] | inst[11:7] | S-immediate
     * +-------------+------------+
     * +----------+---------+-------------+------------+---+
-    * | inst[31] | inst[7] | inst[30:25] | inst[11:8] | 0 | B-immediate 13-bits
+    * | inst[31] | inst[7] | inst[30:25] | inst[11:8] | 0 | B-immediate
     * +----------+---------+-------------+------------+---+
     * +-------------+-----------+
-    * | inst[31:12] | 0(12bits) | U-immediate 32-bits
+    * | inst[31:12] | 0(12bits) | U-immediate
     * +-------------+-----------+
     * +----------+-------------+----------+-------------+---+
-    * | inst[31] | inst[19:12] | inst[20] | inst[30:21] | 0 | J-immediate 21-bits
+    * | inst[31] | inst[19:12] | inst[20] | inst[30:21] | 0 | J-immediate
     * +----------+-------------+----------+-------------+---+
     */
-  val immI = Cat(Fill(52, instSlice(31, 31)), instSlice(31, 20))
-  val immS = Cat(Fill(52, instSlice(31, 31)), instSlice(31, 25), instSlice(11, 7))
+  val immI = instSlice(31, 20)
+  val immS = Cat(instSlice(31, 25), instSlice(11, 7))
   val immB =
-    Cat(Fill(51, instSlice(31, 31)), instSlice(31, 31), instSlice(7, 7), instSlice(30, 25), instSlice(11, 8), 0.U(1.W))
-  val immU = Cat(Fill(32, instSlice(31, 31)), instSlice(31, 12), 0.U(12.W))
+    Cat(instSlice(31, 31), instSlice(7, 7), instSlice(30, 25), instSlice(11, 8), 0.U(1.W))
+  val immU = Cat(instSlice(31, 12), 0.U(12.W))
   val immJ = Cat(
-    Fill(43, instSlice(31, 31)),
     instSlice(31, 31),
     instSlice(19, 12),
     instSlice(20, 20),
@@ -50,11 +50,11 @@ class ImmGen extends Module {
   )
 
   val imm_out_map = Seq(
-    ControlMacro.IMM_TYPE_I -> immI,
-    ControlMacro.IMM_TYPE_S -> immS,
-    ControlMacro.IMM_TYPE_B -> immB,
-    ControlMacro.IMM_TYPE_U -> immU,
-    ControlMacro.IMM_TYPE_J -> immJ
+    ControlMacro.IMM_TYPE_I -> CommonMacro.signExtend(immI),
+    ControlMacro.IMM_TYPE_S -> CommonMacro.signExtend(immS),
+    ControlMacro.IMM_TYPE_B -> CommonMacro.signExtend(immB),
+    ControlMacro.IMM_TYPE_U -> CommonMacro.signExtend(immU),
+    ControlMacro.IMM_TYPE_J -> CommonMacro.signExtend(immJ)
   )
 
   io.imm_out := MuxLookup(io.imm_type, 0.U(64.W))(imm_out_map)
