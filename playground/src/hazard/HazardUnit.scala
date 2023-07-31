6404e62465aa51ae22e7c49038fafedd5676bcb9
22050908-Meng Pei <2398696309@qq.com>
2023-07-31 18:43:21 +0800

    冒险单元

--- playground/src/hazard/HazardUnit.scala
+++ playground/src/hazard/HazardUnit.scala
@@ -3,6 +3,7 @@ package hazard
 import chisel3._
 import entity._
 import control._
+import chisel3.util._
 
 class HazardUnit extends Module {
   val inst_fetch_hazard  = Flipped(IO(new InstFetchHazard))
@@ -20,4 +21,16 @@ class HazardUnit extends Module {
   inst_decode_hazard.enable := not_execute_mem_out
   inst_decode_hazard.reset  := execute_hazard.jump_sig
   execute_hazard.reset      := execute_hazard.jump_sig || !not_execute_mem_out
+
+  def get_fw_rules(rs: UInt) = Seq(
+    (rs === execute_hazard.rd && execute_hazard.wb_ctl === ControlMacro.WB_CTL_ALU) -> HazardMacro.F_CTL_ALU_OUT_E,
+    (rs === execute_hazard.rd && execute_hazard.wb_ctl === ControlMacro.WB_CTL_SNPC) -> HazardMacro.F_CTL_SNPC_E,
+    (rs === load_store_hazard.rd && execute_hazard.wb_ctl === ControlMacro.WB_CTL_ALU) -> HazardMacro.F_CTL_ALU_OUT_M,
+    (rs === load_store_hazard.rd && execute_hazard.wb_ctl === ControlMacro.WB_CTL_MEM) -> HazardMacro.F_CTL_MEM_OUT_M,
+    (rs === load_store_hazard.rd && execute_hazard.wb_ctl === ControlMacro.WB_CTL_SNPC) -> HazardMacro.F_CTL_SNPC_M,
+    (rs === write_back_hazard.rd) -> HazardMacro.F_CTL_WB_DATA
+  )
+
+  inst_decode_hazard.fa_ctl := PriorityMux(get_fw_rules(inst_decode_hazard.rs1))
+  inst_decode_hazard.fb_ctl := PriorityMux(get_fw_rules(inst_decode_hazard.rs2))
 }
