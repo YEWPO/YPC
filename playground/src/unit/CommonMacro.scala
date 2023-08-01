5f057e1fdb2ab507566734911c2c46129be496b0
22050908-Meng Pei <2398696309@qq.com>
2023-08-01 19:55:59 +0800

    Normal Reset Value

--- playground/src/unit/CommonMacro.scala
+++ playground/src/unit/CommonMacro.scala
@@ -4,8 +4,9 @@ import chisel3._
 import chisel3.util._
 
 object CommonMacro {
-  val PC_RESET_VAL   = "h8000_0000".U(64.W)
-  val INST_RESET_VAL = "h13".U(32.W) // NOP instruction
+  val NORMAL_RESET_VAL = 0.U
+  val PC_RESET_VAL     = "h8000_0000".U(64.W)
+  val INST_RESET_VAL   = "h13".U(32.W) // NOP instruction
 
   def signExtend(src: UInt): UInt = {
     val src_len    = src.getWidth
