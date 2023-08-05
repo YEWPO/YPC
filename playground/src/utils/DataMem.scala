8d294cf380844a37ecda412990eaf3f6e9614100
22050908-Meng Pei <2398696309@qq.com>
2023-08-05 16:35:32 +0800

    数据内存

--- playground/src/utils/DataMem.scala
+++ playground/src/utils/DataMem.scala
@@ -62,6 +62,7 @@ class DataMem extends Module {
   val sh_data = Fill(4, CommonMacro.getHalfWord(io.w_data, 0))
   val sw_data = Fill(2, CommonMacro.getWord(io.w_data, 0))
   val sd_data = io.w_data
+
   val w_data_map = Seq(
     ControlMacro.MEM_CTL_SB -> sb_data,
     ControlMacro.MEM_CTL_SH -> sh_data,
@@ -81,4 +82,41 @@ class DataMem extends Module {
   mem_read.io.r_en := r_en
   val r_data = mem_read.io.r_data
 
+  // handle read data
+  val lb_data_map = Seq(
+    0.U -> CommonMacro.getByte(r_data, 0),
+    1.U -> CommonMacro.getByte(r_data, 1),
+    2.U -> CommonMacro.getByte(r_data, 2),
+    3.U -> CommonMacro.getByte(r_data, 3),
+    4.U -> CommonMacro.getByte(r_data, 4),
+    5.U -> CommonMacro.getByte(r_data, 5),
+    6.U -> CommonMacro.getByte(r_data, 6),
+    7.U -> CommonMacro.getByte(r_data, 7)
+  )
+  val lh_data_map = Seq(
+    0.U -> CommonMacro.getHalfWord(r_data, 0),
+    1.U -> CommonMacro.getHalfWord(r_data, 1),
+    2.U -> CommonMacro.getHalfWord(r_data, 2),
+    3.U -> CommonMacro.getHalfWord(r_data, 3)
+  )
+  val lw_data_map = Seq(
+    0.U -> CommonMacro.getWord(r_data, 0),
+    1.U -> CommonMacro.getWord(r_data, 1)
+  )
+  val lb_data = MuxLookup(io.addr(2, 0), 0.U(8.W))(lb_data_map)
+  val lh_data = MuxLookup(io.addr(2, 1), 0.U(16.W))(lh_data_map)
+  val lw_data = MuxLookup(io.addr(2), 0.U(32.W))(lw_data_map)
+  val ld_data = r_data
+
+  // return read data
+  val r_data_map = Seq(
+    ControlMacro.MEM_CTL_LB  -> CommonMacro.signExtend(lb_data),
+    ControlMacro.MEM_CTL_LH  -> CommonMacro.signExtend(lh_data),
+    ControlMacro.MEM_CTL_LW  -> CommonMacro.signExtend(lw_data),
+    ControlMacro.MEM_CTL_LD  -> ld_data,
+    ControlMacro.MEM_CTL_LBU -> CommonMacro.zeroExtend(lb_data),
+    ControlMacro.MEM_CTL_LHU -> CommonMacro.zeroExtend(lh_data),
+    ControlMacro.MEM_CTL_LWU -> CommonMacro.zeroExtend(lw_data)
+  )
+  io.r_data := MuxLookup(io.mem_ctl, 0.U(64.W))(r_data_map)
 }
