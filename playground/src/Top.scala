12b628e13d9d8c80e422afe71fd210796a94cb7c
22050908-Meng Pei <2398696309@qq.com>
2023-08-05 17:37:37 +0800

    将写回合入top

--- playground/src/Top.scala
+++ playground/src/Top.scala
@@ -12,6 +12,7 @@ class Top extends Module {
   val inst_decode_unit = Module(new InstDecodeUnit)
   val execute_unit     = Module(new ExecuteUnit)
   val load_store_unit  = Module(new LoadStoreUnit)
+  val write_back_unit  = Module(new WriteBackUnit)
 
   // ========== Instruction Fetch Unit ============
   inst_fetch_unit.inst_fetch_hazard <> hazard_unit.inst_fetch_hazard
@@ -20,8 +21,10 @@ class Top extends Module {
   // ========== Instruction Decode Unit ============
   inst_decode_unit.inst_decode_hazard <> hazard_unit.inst_decode_hazard
   inst_decode_unit.inst_fetch_data    <> inst_fetch_unit.inst_fetch_data
+  inst_decode_unit.write_back_data    <> write_back_unit.write_back_data
   inst_decode_unit.execute_forward    <> execute_unit.execute_forward
   inst_decode_unit.load_store_forward <> load_store_unit.load_store_forward
+  inst_decode_unit.write_back_forward <> write_back_unit.write_back_forward
 
   // ========== Execute Unit ==========
   execute_unit.inst_decode_data    <> inst_decode_unit.inst_decode_data
@@ -32,6 +35,11 @@ class Top extends Module {
   load_store_unit.execute_data      <> execute_unit.execute_data
   load_store_unit.execute_control   <> execute_unit.execute_control
   load_store_unit.load_store_hazard <> hazard_unit.load_store_hazard
+
+  // ========== Write Back Unit ==========
+  write_back_unit.load_store_data    <> load_store_unit.load_store_data
+  write_back_unit.load_store_control <> load_store_unit.load_store_control
+  write_back_unit.write_back_hazard  <> hazard_unit.write_back_hazard
 
   // ========== Output Test ==========
 }
