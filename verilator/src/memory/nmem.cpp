caa6915a74b79929723ff9227c306a0b5b596a91
22050908-Meng Pei <2398696309@qq.com>
2023-08-08 14:06:59 +0800

    去掉限制每个时钟周期只能使用一次DPI-C的限制

--- verilator/src/memory/nmem.cpp
+++ verilator/src/memory/nmem.cpp
@@ -13,21 +13,11 @@
 extern VTop *top;
 
 void nmem_ifetch(const long long addr, long long *r_data, const svLogic r_en) {
-  static bool pre_clock = false;
-  if (top->clock == pre_clock) return;
-  pre_clock = top->clock;
-  if (top->clock == false) return;
-
   if (!r_en) { *r_data = 0; return; };
   *r_data = pmem_read(addr);
 }
 
 void nmem_read(const long long addr, long long *r_data, const svLogic r_en) {
-  static bool pre_clock = false;
-  if (top->clock == pre_clock) return;
-  pre_clock = top->clock;
-  if (top->clock == false) return;
-
   if (!r_en) { *r_data = 0; return; };
   if (likely(in_pmem(addr))) {
     *r_data = pmem_read(addr);
@@ -35,11 +25,6 @@ void nmem_read(const long long addr, long long *r_data, const svLogic r_en) {
 }
 
 void nmem_write(const long long addr, const long long w_data, const long long mask, const svLogic w_en) {
-  static bool pre_clock = false;
-  if (top->clock == pre_clock) return;
-  pre_clock = top->clock;
-  if (top->clock == false) return;
-
   if (!w_en) return;
   if (likely(in_pmem(addr))) {
     pmem_write(addr, w_data, mask);
