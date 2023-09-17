ee91808a822571d4895f7d032c125455a918e013
22050908-Meng Pei <2398696309@qq.com>
2023-09-17 15:41:52 +0800

    合并CSR的冒险宏到主冒险宏

--- playground/src/macros/HazardMacros.scala
+++ playground/src/macros/HazardMacros.scala
@@ -11,3 +11,10 @@ object HazardMacros {
   val F_CTL_SNPC_M  = "b101".U
   val F_CTL_WB_DATA = "b110".U
 }
+
+object CSRHazardMacros {
+  val CSR_F_CTL_DEFAULT = "b00".U
+  val CSR_F_CTL_EXE     = "b01".U
+  val CSR_F_CTL_LS      = "b10".U
+  val CSR_F_CTL_WB      = "b11".U
+}
