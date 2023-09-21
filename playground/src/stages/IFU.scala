6a74356cd418f878387224ed44b55cc24225ea6f
22050908-Meng Pei <2398696309@qq.com>
2023-09-21 21:38:15 +0800

    WBU握手协议重构

--- playground/src/stages/IFU.scala
+++ playground/src/stages/IFU.scala
@@ -22,9 +22,6 @@ class IFU extends Module {
   /* ========== Input and Output ========== */
   val io = IO(new IFUIO)
 
-  /* ========== Parameter ========== */
-  val s_idle :: s_wait_ready :: Nil = Enum(2)
-
   /* ========== Module ========== */
   val inst_mem = Module(new InstMem)
   val if2id_rst_val = (new IF2IDBundle).Lit(
@@ -37,7 +34,6 @@ class IFU extends Module {
 
   /* ========== Register ========== */
   val pc      = RegInit(CommonMacros.PC_RESET_VAL)
-  val r_state = RegInit(s_idle)
   val r_if2id = RegInit(if2id_rst_val)
   val r_valid = RegInit(false.B)
 
@@ -53,28 +49,21 @@ class IFU extends Module {
     CommonMacros.getWord(inst_mem.io.r_data, 1),
     CommonMacros.getWord(inst_mem.io.r_data, 0)
   )
+  val valid_enable = !io.if2id.valid || io.if2id.ready
+  val valid_next   = r_valid && !io.if2id.ready
 
   /* ========== Sequential Circuit ========== */
-  when(r_state === s_idle) {
-    r_state := Mux(io.if2id.valid, s_wait_ready, s_idle)
+  r_valid := Mux(valid_enable, true.B, valid_next)
 
-    r_valid := true.B
+  r_if2id.data.pc   := pc
+  r_if2id.data.snpc := snpc
+  r_if2id.data.inst := inst
 
-    r_if2id.data.pc   := pc
-    r_if2id.data.snpc := snpc
-    r_if2id.data.inst := inst
-  }.elsewhen(r_state === s_wait_ready) {
-    r_state := Mux(io.if2id.fire, s_idle, s_wait_ready)
-
-    r_valid := Mux(io.if2id.fire, false.B, r_valid)
-
-    r_if2id := Mux(io.if2id.fire, if2id_rst_val, r_if2id)
-
-    pc := Mux(io.if2id.fire, npc, pc)
-  }
+  pc := Mux(io.if2id.fire, npc, pc)
 
   /* ========== Combinational Circuit ========== */
   inst_mem.io.addr := pc
 
+  io.if2id.valid     := r_valid
   io.if2id.bits.data := r_if2id.data
 }
--- playground/src/stages/WBU.scala
+++ playground/src/stages/WBU.scala
@@ -9,10 +9,7 @@ import bundles.writeback._
 import utils.writeback._
 
 class WBUIO extends Bundle {
-  val in = Input(new Bundle {
-    val data    = new LS2WBDataBundle
-    val control = new LS2WBControlBundle
-  })
+  val ls2wb = Flipped(Decoupled(new LS2WBBundle))
   val out = Output(new Bundle {
     val data       = Output(new WB2RegBundle)
     val hazard     = Output(new WBHazardDataBundle)
@@ -27,6 +24,32 @@ class WBU extends Module {
   /* ========== Module ========== */
   val statistic = Module(new Statistic)
 
+  /* ========== Parameter ========== */
+  val ls2wb_rst_val = (new LS2WBBundle).Lit(
+    _.data -> (new LS2WBDataBundle).Lit(
+      _.snpc       -> CommonMacros.PC_RESET_VAL,
+      _.pc         -> CommonMacros.PC_RESET_VAL,
+      _.dnpc       -> CommonMacros.PC_RESET_VAL,
+      _.inst       -> CommonMacros.INST_RESET_VAL,
+      _.rd         -> 0.U,
+      _.mem_out    -> 0.U,
+      _.exe_out    -> 0.U,
+      _.csr_w_data -> 0.U,
+      _.csr_w_addr -> 0.U
+    ),
+    _.control -> (new LS2WBControlBundle).Lit(
+      _.wb_ctl     -> ControlMacros.WB_CTL_DEFAULT,
+      _.reg_w_en   -> ControlMacros.REG_W_DISABLE,
+      _.ebreak_op  -> ControlMacros.EBREAK_OP_NO,
+      _.invalid_op -> ControlMacros.INVALID_OP_NO,
+      _.csr_w_en   -> false.B
+    )
+  )
+
+  /* ========== Wire ========== */
+  val ready_next = io.ls2wb.valid
+  val ls2wb_data = Wire(new LS2WBBundle)
+
   /* ========== Register ========== */
   val r_statistic = RegInit(
     (new StatisticBundle).Lit(
@@ -39,32 +62,36 @@ class WBU extends Module {
   )
 
   /* ========== Sequential Circuit ========== */
-  r_statistic.ebreak_op  := io.in.control.ebreak_op
-  r_statistic.invalid_op := io.in.control.invalid_op
-  r_statistic.pc         := io.in.data.pc
-  r_statistic.dnpc       := io.in.data.dnpc
-  r_statistic.inst       := io.in.data.inst
+  r_statistic.ebreak_op  := ls2wb_data.control.ebreak_op
+  r_statistic.invalid_op := ls2wb_data.control.invalid_op
+  r_statistic.pc         := ls2wb_data.data.pc
+  r_statistic.dnpc       := ls2wb_data.data.dnpc
+  r_statistic.inst       := ls2wb_data.data.inst
 
   /* ========== Combinational Circuit ========== */
+  ls2wb_data := Mux(io.ls2wb.valid, io.ls2wb.bits, ls2wb_rst_val)
+
+  io.ls2wb.ready := ready_next
+
   statistic.io.in := r_statistic
 
-  io.out.data.wb_data := MuxLookup(io.in.control.wb_ctl, 0.U(64.W))(
+  io.out.data.wb_data := MuxLookup(ls2wb_data.control.wb_ctl, 0.U(64.W))(
     Seq(
-      ControlMacros.WB_CTL_ALU  -> io.in.data.exe_out,
-      ControlMacros.WB_CTL_MEM  -> io.in.data.mem_out,
-      ControlMacros.WB_CTL_SNPC -> io.in.data.snpc
+      ControlMacros.WB_CTL_ALU  -> ls2wb_data.data.exe_out,
+      ControlMacros.WB_CTL_MEM  -> ls2wb_data.data.mem_out,
+      ControlMacros.WB_CTL_SNPC -> ls2wb_data.data.snpc
     )
   )
-  io.out.data.rd       := io.in.data.rd
-  io.out.data.reg_w_en := io.in.control.reg_w_en
+  io.out.data.rd       := ls2wb_data.data.rd
+  io.out.data.reg_w_en := ls2wb_data.control.reg_w_en
 
-  io.out.data.csr_w_addr := io.in.data.csr_w_addr
-  io.out.data.csr_w_data := io.in.data.csr_w_data
-  io.out.data.csr_w_en   := io.in.control.csr_w_en
+  io.out.data.csr_w_addr := ls2wb_data.data.csr_w_addr
+  io.out.data.csr_w_data := ls2wb_data.data.csr_w_data
+  io.out.data.csr_w_en   := ls2wb_data.control.csr_w_en
 
-  io.out.hazard.rd                 := io.in.data.rd
-  io.out.hazard.rd_tag             := io.in.control.reg_w_en
-  io.out.hazard.wb_ctl             := io.in.control.wb_ctl
-  io.out.csr_hazard.csr_w_addr     := io.in.data.csr_w_addr
-  io.out.csr_hazard.csr_w_addr_tag := io.in.control.csr_w_en
+  io.out.hazard.rd                 := ls2wb_data.data.rd
+  io.out.hazard.rd_tag             := ls2wb_data.control.reg_w_en
+  io.out.hazard.wb_ctl             := ls2wb_data.control.wb_ctl
+  io.out.csr_hazard.csr_w_addr     := ls2wb_data.data.csr_w_addr
+  io.out.csr_hazard.csr_w_addr_tag := ls2wb_data.control.csr_w_en
 }
