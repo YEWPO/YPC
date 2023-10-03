5dd3a31c2cc4633001267dcdb9894cff690916a7
22050908-Meng Pei <2398696309@qq.com>
2023-10-03 14:12:05 +0800

    instram disable arready register

--- playground/src/utils/instfetch/InstRAM.scala
+++ playground/src/utils/instfetch/InstRAM.scala
@@ -15,24 +15,21 @@ class InstRAM extends Module {
   val inst_mem = Module(new InstMem)
 
   /* ========== Register ========== */
-  val r_arready = RegInit(false.B)
-  val r_rdata   = RegInit(0.U(64.W))
-  val r_rvalid  = RegInit(false.B)
+  val r_rdata  = RegInit(0.U(64.W))
+  val r_rvalid = RegInit(false.B)
 
   /* ========== Wire ========== */
-  val arready_enable = io.ar.valid && !io.ar.ready && (!io.r.valid || io.r.ready)
-  val rvalid_enable  = arready_enable
-  val rvalid_next    = r_rvalid && !io.r.ready
+  val rvalid_enable = io.ar.valid && (!io.r.valid || io.r.ready)
+  val rvalid_next   = r_rvalid && !io.r.ready
 
   /* ========== Sequential Circuit ========== */
-  r_arready := Mux(arready_enable, true.B, false.B)
-  r_rvalid  := Mux(rvalid_enable, true.B, rvalid_next)
-  r_rdata   := inst_mem.io.r_data
+  r_rvalid := Mux(rvalid_enable, true.B, rvalid_next)
+  r_rdata  := inst_mem.io.r_data
 
   /* ========== Combinational Circuit ========== */
   inst_mem.io.addr := io.ar.bits.addr
 
-  io.ar.ready    := r_arready
+  io.ar.ready    := io.ar.valid && (!io.r.valid || io.r.ready)
   io.r.bits.resp := 0.U(2.W)
   io.r.valid     := r_rvalid
   io.r.bits.data := r_rdata
