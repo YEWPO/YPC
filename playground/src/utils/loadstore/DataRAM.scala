734bc908348fda03a7baf96a82f560de4c1ceec1
22050908-Meng Pei <2398696309@qq.com>
2023-10-04 19:22:18 +0800

    内存数据写

--- playground/src/utils/loadstore/DataRAM.scala
+++ playground/src/utils/loadstore/DataRAM.scala
@@ -24,19 +24,37 @@ class DataRAM extends Module {
   val r_rvalid = RegInit(false.B)
   val r_rdata  = RegInit(0.U(64.W))
 
+  val r_bvalid = RegInit(false.B)
+
   /* ========== Wire ========== */
   val rvalid_enable = io.ar.valid && (!io.r.valid || io.r.ready)
   val rvalid_next   = r_rvalid && !io.r.ready
+
+  val bvalid_enable = io.aw.valid && io.w.valid && (!io.b.valid || io.b.ready)
+  val bvalid_next   = r_bvalid && !io.b.ready
 
   /* ========== Sequential Cicuit ========== */
   r_rvalid := Mux(rvalid_enable, true.B, rvalid_next)
   r_rdata  := mem_read.io.r_data
 
+  r_bvalid := Mux(bvalid_enable, true.B, bvalid_next)
+
   /* ========== Combinational Cicuit ========== */
   mem_read.io.addr := io.ar.bits.addr
 
-  io.ar.ready    := io.ar.valid && (!io.r.valid || io.r.ready)
+  mem_write.io.addr   := io.aw.bits.addr
+  mem_write.io.mask   := io.w.bits.strb
+  mem_write.io.w_data := io.w.bits.data
+
+  io.ar.ready := io.ar.valid && (!io.r.valid || io.r.ready)
+
   io.r.bits.resp := 0.U(2.W)
   io.r.valid     := r_rvalid
   io.r.bits.data := r_rdata
+
+  io.aw.ready := io.aw.valid && io.w.valid && (!io.b.valid || io.b.ready)
+
+  io.w.ready := io.aw.valid && io.w.valid && (!io.b.valid || io.b.ready)
+
+  io.b.valid := r_bvalid
 }
