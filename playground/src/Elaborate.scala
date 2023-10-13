a235914f37d5b2049fa67dfebcb34bec0b5963ae
22050908-Meng Pei <2398696309@qq.com>
2023-10-13 18:08:06 +0800

    更新firtool和Elborate，无需单独写脚本

--- playground/src/Elaborate.scala
+++ playground/src/Elaborate.scala
@@ -1,7 +1,18 @@
 import circt.stage._
 
 object Elaborate extends App {
-  def top       = new Top
-  val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+  def top = new Top
+  val options = Seq(
+    FirtoolOption(
+      "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket"
+    ),
+    FirtoolOption("--split-verilog"),
+    FirtoolOption("-o=build/verilog_dir/gen"),
+    FirtoolOption("--disable-all-randomization")
+  )
+  val generator = Seq(
+    chisel3.stage.ChiselGeneratorAnnotation(() => top),
+    CIRCTTargetAnnotation(CIRCTTarget.SystemVerilog)
+  ) ++ options
+  (new ChiselStage).execute(args, generator)
 }
