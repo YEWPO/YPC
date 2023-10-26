4dca813fdbee09e33a9019523b7abacd39b6faaa
22050908-Meng Pei <2398696309@qq.com>
2023-10-26 15:30:50 +0800

    SRAM读逻辑更新

--- playground/src/utils/sram/SRAM.scala
+++ playground/src/utils/sram/SRAM.scala
@@ -21,13 +21,17 @@ class SRAM extends Module {
   val mem_write = Module(new MemWrite)
 
   /* ========== Register ========== */
+  val r_araddr = RegInit(0.U(64.W))
+  val r_arready = RegInit(false.B)
+
   val r_rvalid = RegInit(false.B)
   val r_rdata  = RegInit(0.U(64.W))
 
   val r_bvalid = RegInit(false.B)
 
   /* ========== Wire ========== */
-  val rvalid_enable = io.ar.valid && (!io.r.valid || io.r.ready)
+  val r_en = io.ar.valid && r_arready && !r_rvalid
+
   val rvalid_next   = r_rvalid && !io.r.ready
   val rdata_next    = Mux(io.r.fire, 0.U(64.W), r_rdata)
 
@@ -35,19 +39,22 @@ class SRAM extends Module {
   val bvalid_next   = r_bvalid && !io.b.ready
 
   /* ========== Sequential Cicuit ========== */
-  r_rvalid := Mux(rvalid_enable, true.B, rvalid_next)
-  r_rdata  := Mux(rvalid_enable, mem_read.io.r_data, rdata_next)
+  r_araddr := Mux(io.ar.valid && !r_arready, io.ar.bits.addr, r_araddr)
+  r_arready := Mux(io.ar.valid && !r_arready, true.B, false.B);
+
+  r_rvalid := Mux(r_en, true.B, rvalid_next)
+  r_rdata  := Mux(r_en, mem_read.io.r_data, rdata_next)
 
   r_bvalid := Mux(bvalid_enable, true.B, bvalid_next)
 
   /* ========== Combinational Cicuit ========== */
-  mem_read.io.addr := io.ar.bits.addr
+  mem_read.io.addr := r_araddr
 
   mem_write.io.addr   := io.aw.bits.addr
   mem_write.io.mask   := io.w.bits.strb
   mem_write.io.w_data := io.w.bits.data
 
-  io.ar.ready := io.ar.valid && (!io.r.valid || io.r.ready)
+  io.ar.ready := r_arready
 
   io.r.valid     := r_rvalid
   io.r.bits.data := r_rdata
