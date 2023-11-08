d356a9e87c9bbe03a191905c225ddc8dfcdbde64
22050908-Meng Pei <2398696309@qq.com>
2023-11-08 16:04:05 +0800

    完整乘法器生成器

--- playground/src/utils/execute/Mul.scala
+++ playground/src/utils/execute/Mul.scala
@@ -3,15 +3,15 @@ package utils.execute
 import chisel3._
 import chisel3.util._
 
-class GenPartSummandIO(xlen: Int) extends Bundle {
-  val src     = Input(UInt(xlen.W))
+class GenPartSummandIO(len: Int) extends Bundle {
+  val src     = Input(UInt(len.W))
   val booth   = Input(UInt(3.W))
-  val summand = Output(UInt(xlen.W))
+  val summand = Output(UInt(len.W))
   val carry   = Output(Bool())
 }
 
-class GenPartSummand(xlen: Int) extends Module {
-  val io = IO(new GenPartSummandIO(xlen))
+class GenPartSummand(len: Int) extends Module {
+  val io = IO(new GenPartSummandIO(len))
 
   val src = Cat(io.src, 0.U(1.W))
 
@@ -20,9 +20,9 @@ class GenPartSummand(xlen: Int) extends Module {
   val addTwice   = ~io.booth(2) & io.booth(1) & io.booth(0)
   val minusTwice = io.booth(2) & ~io.booth(1) & ~io.booth(0)
 
-  val summand = Wire(Vec(xlen, UInt(1.W)))
+  val summand = Wire(Vec(len, UInt(1.W)))
 
-  for (i <- 0 until xlen) {
+  for (i <- 0 until len) {
     summand(i) := (addOnce & src(i + 1)) |
       (minusOnce & ~src(i + 1)) |
       (addTwice & src(i)) |
@@ -34,11 +34,38 @@ class GenPartSummand(xlen: Int) extends Module {
   io.carry := io.booth(2) & ~(io.booth(1) & io.booth(0))
 }
 
-class MulIO(xlen: Int) extends Bundle {
-  val src = Input(UInt(xlen.W))
-  val out = Output(UInt(xlen.W))
+class GenPartSummandsIO(len: Int) extends Bundle {
+  val multiplicand = Input(UInt(len.W))
+  val multiplier   = Input(UInt(len.W))
+
+  val summands = Output(Vec(len / 2, UInt((2 * len).W)))
+  val carrys   = Output(Vec(len / 2, Bool()))
 }
 
-class Mul(xlen: Int) extends Module {
-  val io = IO(new MulIO(xlen))
+class GenPartSummands(len: Int) extends Module {
+  val io = IO(new GenPartSummandsIO(len))
+
+  val multiplicand = Cat(Fill(len, io.multiplicand(len - 1)), io.multiplicand)
+  val multiplier = Cat(io.multiplier, 0.U(1.W))
+
+  for (i <- len - 1 to 1 by -2) {
+    val genPartSummand = Module(new GenPartSummand(2 * len))
+
+    genPartSummand.io.src   := multiplicand
+    genPartSummand.io.booth := multiplier(i + 1, i - 1)
+
+    io.summands((i - 1) / 2) := genPartSummand.io.summand
+    io.carrys((i - 1) / 2)   := genPartSummand.io.carry
+  }
+}
+
+class MulIO(len: Int) extends Bundle {
+  val multiplicand = Input(UInt(len.W))
+  val multiplier   = Input(UInt(len.W))
+
+  val answer = Output(UInt((2 * len).W))
+}
+
+class Mul(len: Int) extends Module {
+  val io = IO(new MulIO(len))
 }
